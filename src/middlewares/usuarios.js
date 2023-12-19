de78afa2eeaf08cdf5e0ee569e2e294bbe2eab3e
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-19 18:36:51 -0300

    feat: cria middleware de validação do token

--- src/middlewares/usuarios.js
+++ src/middlewares/usuarios.js
@@ -1,5 +1,35 @@
 const pool = require('../database/conexao');
+const jwt = require('jsonwebtoken');
 
 usuariosMiddlewares = {};
+
+usuariosMiddlewares.validaToken = async (req, res, next) => {
+    const { authorizations } = req.headers;
+
+    try {
+        if (!authorizations) {
+            return res.status(401).json({ mensagem: 'Usuário não autorizado.' });
+        }
+
+        const token = authorizations.split(' ')[1];
+
+        const { id } = jwt.verify(token, process.env.JWT_PASSWORD);
+
+        const { rows, rowCount } = await pool.query('select * from usuarios where id = $1;', [id]);
+
+        if (rowCount === 0) {
+            return res.status(401).json({ mensagem: 'Usuário não autorizado.' });
+        }
+
+        const { senha, ...usuario } = rows[0];
+
+        req.usuario = usuario;
+
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ mensagem: 'erro interno no servidor' });
+    }
+}
 
 module.exports = usuariosMiddlewares;
--- src/rotas.js
+++ src/rotas.js
@@ -3,10 +3,14 @@ const express = require('express');
 const rotas = express();
 
 const { cadastrar, listar, login } = require('./controllers/usuarios');
-const { } = require('./middlewares/usuarios');
+const { validaToken } = require('./middlewares/usuarios');
 
 rotas.get('/usuarios', listar);
 rotas.post('/usuarios', cadastrar);
 rotas.post('/login', login);
+
+rotas.use(validaToken);
+
+
 
 module.exports = rotas;
