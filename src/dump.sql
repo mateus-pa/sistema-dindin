6ef6261b7eb699fc5dce8400522046dcb760667e
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-19 18:24:28 -0300

    feat: cria script de criação do banco de dados e tabelas

---
+++ src/dump.sql
@@ -0,0 +1,24 @@
+-- BD DINDIN
+CREATE DATABASE DINDIN;
+
+CREATE TABLE IF NOT EXISTS usuarios(
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  senha TEXT
+);
+
+CREATE TABLE IF NOT EXISTS categorias(
+  id SERIAL PRIMARY KEY,
+  descricao TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transacoes(
+  id SERIAL NOT NULL,
+  descricao TEXT NOT NULL,
+  valor INTEGER NOT NULL,
+  data DATE,
+  tipo VARCHAR(20),
+  categoria_id INTEGER REFERENCES categorias(id),
+  usuario_id INTEGER REFERENCES usuarios(id)
+);
