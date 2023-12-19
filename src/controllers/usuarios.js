1611aa4c2ed43d2ca4b6cd73dd1a2f2af42b1a41
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-19 18:19:05 -0300

    feat: cria rota usuarios e função cadastrar

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,5 +1,38 @@
 const pool = require('../database/conexao');
 
-module.exports = {
+const cadastrar = async function (req, res) {
+    const { nome, email, senha } = req.body;
 
+    try {
+        if (!nome) {
+            return res.status(400).json({ mensagem: 'O campo nome é obrigatório.' });
+        }
+
+        if (!email) {
+            return res.status(400).json({ mensagem: 'O campo email é obrigatório.' });
+        }
+
+        if (!senha) {
+            return res.status(400).json({ mensagem: 'O campo senha é obrigatório.' });
+        }
+
+        const emailExiste = await pool.query('select count(*) from usuarios where email = $1;', [email]);
+
+        if (emailExiste.rows[0].count !== 0) {
+            return res.status(400).json({ mensagem: 'Este email já existe.' });
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const novoUsuario = await pool.query('insert into usuarios (nome, email, senha) values ($1, $2, $3) returning id, nome, email;', [nome, email, senhaCriptografada]);
+
+        return res.status(201).json(novoUsuario.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ mensagem: 'erro interno no servidor' });
+    }
+}
+
+module.exports = {
+    cadastrar
 }
--- src/rotas.js
+++ src/rotas.js
@@ -5,4 +5,6 @@ const rotas = express();
 const usuariosController = require('./controllers/usuarios');
 const usuariosMiddleware = require('./middlewares/usuarios');
 
+rotas.post('/usuarios', usuariosController.cadastrar);
+
 module.exports = rotas;
