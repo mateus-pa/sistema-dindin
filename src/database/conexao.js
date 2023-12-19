54a294cb51b8cbbeda2c7b3c2e36a8083ed4503b
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-19 18:27:14 -0300

    feat: atualiza a importação do dotenv

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg');
 
-require('dotenv').config();
-
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
