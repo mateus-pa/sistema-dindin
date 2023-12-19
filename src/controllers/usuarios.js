085ee3676e820f4049e2d2661e764fbd629b3cfe
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-19 18:50:22 -0300

    feat: cria rota para atualizar usuário autenticado

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -35,7 +35,7 @@ usuariosController.cadastrar = async function (req, res) {
     );
 
     if (parseInt(emailExiste.rows[0].count) !== 0) {
-      return res.status(400).json({ mensagem: 'Este email já existe.' });
+      return res.status(400).json({ mensagem: 'O e-mail informado já está sendo utilizado por outro usuário.' });
     }
 
     const senhaCriptografada = await bcrypt.hash(senha, 10);
@@ -94,6 +94,47 @@ usuariosController.detalhar = async (req, res) => {
   const usuario = req.usuario;
 
   return res.status(200).json(usuario);
+}
+
+usuariosController.atualizar = async (req, res) => {
+  const { nome, email, senha } = req.body;
+  const usuario = req.usuario;
+
+  try {
+    if (!nome) {
+      return res.status(400).json({ mensagem: 'O campo nome é obrigatório.' });
+    }
+
+    if (!email) {
+      return res.status(400).json({ mensagem: 'O campo email é obrigatório.' });
+    }
+
+    if (!senha) {
+      return res.status(400).json({ mensagem: 'O campo senha é obrigatório.' });
+    }
+
+    const emailExiste = await pool.query(
+      'select count(*) from usuarios where email = $1;',
+      [email]
+    );
+
+    if (parseInt(emailExiste.rows[0].count) !== 0) {
+      return res.status(400).json({ mensagem: 'O e-mail informado já está sendo utilizado por outro usuário.' });
+    }
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    await pool.query(`update usuarios
+    set nome = $1,
+    email = $2,
+    senha = $3
+    where id = $4;`, [nome, email, senhaCriptografada, usuario.id]);
+
+    return res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ mensagem: 'erro interno no servidor' });
+  }
 }
 
 module.exports = usuariosController;
--- src/rotas.js
+++ src/rotas.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const rotas = express();
 
-const { cadastrar, listar, login, detalhar } = require('./controllers/usuarios');
+const {
+    cadastrar,
+    listar,
+    login,
+    detalhar,
+    atualizar,
+} = require('./controllers/usuarios');
+
 const { validaToken } = require('./middlewares/usuarios');
 
 rotas.get('/usuarios', listar);
@@ -12,5 +19,6 @@ rotas.post('/login', login);
 rotas.use(validaToken);
 
 rotas.get('/usuario', detalhar);
+rotas.put('/usuario', atualizar);
 
 module.exports = rotas;
